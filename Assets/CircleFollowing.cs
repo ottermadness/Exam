50d9d5318fe990e7986026a13c910f4c50b91003
ottermadness <crimeofdesign@gmail.com>
2014-03-10 21:02:53 +0000

    Here we go
    YAY

---
+++ Assets/CircleFollowing.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class CircleFollowing : MonoBehaviour {
+
+	float mass = 1.0f;
+	float maxSpeed = 5.0f;
+	Vector3 velocity = Vector3.zero;
+	Vector3 force = Vector3.zero;
+	int currentWaypoint = 0;
+
+	Vector3 target = Vector3.zero;
+
+	Vector3 [] waypoints  = new Vector3 []
+	{
+		new Vector3(0,0,10),
+		new Vector3(4,0,6),
+		new Vector3(8,0,2),
+		new Vector3(8,0,-2),
+		new Vector3(4,0,-6),
+		new Vector3(0,0,-10),
+		new Vector3(-4,0,-6),
+		new Vector3(-8,0,-2),
+		new Vector3(-8,0,2),
+		new Vector3(-4,0,6)
+
+	};
+
+	Vector3 Seek (Vector3 target)
+	{
+		Vector3 toTarget = target-transform.position;
+		if(toTarget.magnitude<1.0f)
+		{
+			currentWaypoint++;
+				
+			if(currentWaypoint == waypoints.Length)
+				{
+					currentWaypoint = 0;
+				}
+		}
+
+		toTarget.Normalize ();
+		toTarget*=maxSpeed;
+
+		return toTarget-velocity;
+	}
+
+	private void DrawPath()
+	{
+		Debug.DrawLine (waypoints[0],waypoints[1]);
+		Debug.DrawLine (waypoints[1],waypoints[2]);
+		Debug.DrawLine (waypoints[2],waypoints[3]);
+		Debug.DrawLine (waypoints[3],waypoints[4]);
+		Debug.DrawLine (waypoints[4],waypoints[5]);
+		Debug.DrawLine (waypoints[5],waypoints[6]);
+		Debug.DrawLine (waypoints[6],waypoints[7]);
+		Debug.DrawLine (waypoints[7],waypoints[8]);
+		Debug.DrawLine (waypoints[8],waypoints[9]);
+		Debug.DrawLine (waypoints[9],waypoints[0]);
+	}
+
+	void Update ()
+	{
+		target = waypoints [currentWaypoint];
+		force += Seek (target);
+
+		Vector3 accel = force / mass;
+
+		velocity = velocity + accel * Time.deltaTime;
+		transform.position=transform.position + accel * Time.deltaTime;
+		force = Vector3.zero;
+		if(velocity.magnitude>float.Epsilon)
+		{
+			transform.forward=Vector3.Normalize (velocity);
+		}
+		velocity *= .99f;
+	}
+}
---
+++ Assets/RandomCube.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomCube : MonoBehaviour {
+
+	Vector3 velocity = Vector3.zero;
+	Vector3 force = Vector3.zero;
+	float maxSpeed = 2.0f;
+	float mass = 1.0f;
+
+	public Vector3 target = Vector3.zero;
+
+	Vector3 ArriveAtTarget (Vector3 target)
+	{
+		Vector3 toTarget = target - transform.position;
+		float arriveDist = 1.0f;
+		float decel = 1.0f;
+		if(toTarget.magnitude<1.0f)
+		{
+			return Vector3.zero;
+		}
+		else
+		{
+			float rSpeed = maxSpeed*(toTarget.magnitude/(decel*arriveDist));
+			float cSpeed = Mathf.Min (maxSpeed,rSpeed);
+			toTarget*=cSpeed;
+
+			return toTarget - velocity;
+		}
+
+	}
+
+	void Start ()
+	{
+		float a = Random.Range (-10, 10);
+		float b = Random.Range (-10, 10);
+
+		float e=0;
+		float d=0;
+
+		if (a<0)
+		{
+			d = a*-1;
+		}
+
+		if (b<0)
+		{
+			e = b*-1;
+		}
+
+		if(d+e > 10)
+		{
+			float c = d+e - 10;
+			if(d>e)
+			{
+				if(a>0)
+				{
+					a=a-c;
+				}
+				if(a<0)
+				{
+					a=a+c;
+				}
+			}
+			else if(d<e)
+			{
+				if(b>0)
+				{
+					b=b-c;
+				}
+				if(b<0)
+				{
+					b=b+c;
+				}
+			}
+		}
+
+		target = new Vector3 (a,0,b);
+	}
+
+	void Update ()
+	{
+		force += ArriveAtTarget (target);
+		
+		Vector3 accel = force / mass;
+		
+		velocity = velocity + accel * Time.deltaTime;
+		transform.position=transform.position + accel * Time.deltaTime;
+		if (force.magnitude<1f)
+		{
+			Start ();
+		}
+		force = Vector3.zero;
+		if(velocity.magnitude>float.Epsilon)
+		{
+			transform.forward=Vector3.Normalize (velocity);
+		}
+		velocity *= .99f;
+	}
+}
